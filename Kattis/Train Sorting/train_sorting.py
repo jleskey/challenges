17f8296fef625ba30eb8edd5732dd1a797e352bf
Joseph Leskey <joseph@josephleskey.com>
2023-11-01 12:33:50 -0400

    Try something with a mean-sorted list

--- Kattis/Train Sorting/train_sorting.py
+++ Kattis/Train Sorting/train_sorting.py
@@ -9,8 +9,20 @@ front = 0 # heaviest
 back = 0 # lightest
 count = 0
 
+incoming_train_weights = []
+mean_sorted_weights = []
+
 for i in range(train_count):
     weight = int(test_file.readline().strip())
+    incoming_train_weights.append(weight)
+
+mean = sum(incoming_train_weights) / len(incoming_train_weights)
+
+mean_sorted_weights = sorted(incoming_train_weights, key=lambda weight: abs(mean - weight), reverse=True)
+
+def place(weight):
+    global front, back, count
+
     if weight > front:
         count += 1
         front = weight
@@ -19,5 +31,15 @@ for i in range(train_count):
     elif weight < back:
         count += 1
         back = weight
+
+for weight in incoming_train_weights:
+    first_pair = mean_sorted_weights[0:2]
+    lightest = min(first_pair)
+    heaviest = max(first_pair)
+
+    if len(mean_sorted_weights) == 1 or (weight >= lightest and weight <= heaviest):
+        place(weight)
+
+    mean_sorted_weights.remove(weight)
 
 print(count)
